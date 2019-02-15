894acea489864a04000836d023f15a1d4b12b76f
pjay79 <praveen.jayarajan@gmail.com>
2019-02-16 10:52:56 +1100

    Update Footer and Form

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,26 +5,10 @@ import AppBar from '@material-ui/core/AppBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
 import SearchIcon from '@material-ui/icons/Search';
 import MoreIcon from '@material-ui/icons/MoreVert';
 
 const styles = theme => ({
-  text: {
-    paddingTop: theme.spacing.unit * 2,
-    paddingLeft: theme.spacing.unit * 2,
-    paddingRight: theme.spacing.unit * 2,
-  },
-  paper: {
-    paddingBottom: 50,
-  },
-  list: {
-    marginBottom: theme.spacing.unit * 2,
-  },
-  subHeader: {
-    backgroundColor: theme.palette.background.paper,
-  },
   appBar: {
     top: 'auto',
     bottom: 0,
@@ -32,14 +16,6 @@ const styles = theme => ({
   toolbar: {
     alignItems: 'center',
     justifyContent: 'space-between',
-  },
-  fabButton: {
-    position: 'absolute',
-    zIndex: 1,
-    top: -30,
-    left: 0,
-    right: 0,
-    margin: '0 auto',
   },
 });
 
@@ -51,9 +27,6 @@ function BottomAppBar(props) {
       <AppBar position="fixed" color="primary" className={classes.appBar}>
         <Toolbar className={classes.toolbar}>
           <IconButton color="inherit" aria-label="Open drawer" />
-          <Fab color="secondary" aria-label="Add" className={classes.fabButton}>
-            <AddIcon />
-          </Fab>
           <div>
             <IconButton color="inherit">
               <SearchIcon />
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import FormControl from '@material-ui/core/FormControl';
 import TextField from '@material-ui/core/TextField';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
 
 const styles = theme => ({
   container: {
@@ -16,19 +18,35 @@ const styles = theme => ({
   formControl: {
     margin: theme.spacing.unit * 4,
   },
+  fabWrapper: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fab: {
+    margin: theme.spacing.unit,
+  },
 });
 
-class ComposedTextField extends React.Component {
+class ComposedTextField extends Component {
   state = {
-    name: 'Composed TextField',
+    name: '',
+    vendor: '',
+    cost: 0,
+    confirmed: false,
   };
 
-  componentDidMount() {
-    this.forceUpdate();
-  }
-
   handleChange = event => {
-    this.setState({ name: event.target.value });
+    this.setState({ [event.target.name]: event.target.value }, () => {
+      console.log(this.state);
+    });
+  };
+
+  handleSubmit = event => {
+    alert('New expense added:');
+    event.preventDefault();
   };
 
   render() {
@@ -37,19 +55,58 @@ class ComposedTextField extends React.Component {
     return (
       <div className={classes.container}>
         <Paper>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <FormControl className={classes.formControl} variant="outlined">
-              <TextField id="time" type="text" label="Name" />
+              <TextField
+                id="time"
+                type="text"
+                label="Name"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
             </FormControl>
             <FormControl className={classes.formControl} variant="outlined">
-              <TextField id="time" type="text" label="Vendor" />
+              <TextField
+                id="time"
+                type="text"
+                label="Vendor"
+                name="vendor"
+                value={this.state.vendor}
+                onChange={this.handleChange}
+              />
             </FormControl>
             <FormControl className={classes.formControl} variant="outlined">
-              <TextField id="time" type="text" label="Cost" />
+              <TextField
+                id="time"
+                type="text"
+                label="Cost"
+                name="cost"
+                value={this.state.cost}
+                onChange={this.handleChange}
+              />
             </FormControl>
             <FormControl className={classes.formControl} variant="outlined">
-              <TextField id="time" type="text" label="Confirmed" />
+              <TextField
+                id="time"
+                type="text"
+                label="Confirmed"
+                name="confirmed"
+                value={this.state.confirmed}
+                onChange={this.handleChange}
+              />
             </FormControl>
+            <Paper className={classes.fabWrapper}>
+              <Fab
+                color="secondary"
+                aria-label="Add"
+                className={classes.fab}
+                type="submit"
+                value="Submit"
+              >
+                <AddIcon />
+              </Fab>
+            </Paper>
           </form>
         </Paper>
       </div>
